234a7461539779c00ed795bd048e30970ff74ef6
nithin <158035569+nithinSai98@users.noreply.github.com>
2024-11-19 21:14:39 +0530

    models.py + permissions.py --> CODE ADDED

--- backend/intranet_project/app_intranet/models.py
+++ backend/intranet_project/app_intranet/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class user(AbstractUser):
+    
+    role_choices = (
+        ('admin', 'Admin'),
+        ('faculty', 'Faculty'),
+        ('student', 'Student'),
+    )
+    role = models.CharField(max_length=20, choices=role_choices)
